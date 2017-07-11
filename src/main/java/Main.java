40b1c2b8746fde6f050051d0f780d7c79f01b67a
time2die <time2java@gmail.com>
2017-07-12 00:24:41 +0300

    add comands

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
@@ -7,7 +8,6 @@ import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
-import com.typesafe.config.ConfigFactory;
 
 import java.util.logging.Logger;
 
@@ -19,13 +19,14 @@ import java.util.logging.Logger;
 public class Main {
     private static final Logger logger = Logger.getLogger("tRussianBank");
 
-    private final String tgKey ;
-    Main(){
-        tgKey =  initConfiguration() ;
-        initTgListener() ;
+    private final String tgKey;
+
+    Main() {
+        tgKey = initConfiguration();
+        initTgListener();
     }
 
-    private String  initConfiguration() {
+    private String initConfiguration() {
         Config conf = ConfigFactory.load();
         return conf.getString("tgBotKey");
     }
@@ -42,23 +43,45 @@ public class Main {
     }
 
     public static void main(String[] args) throws TelegramApiRequestException {
-        new Main() ;
+        new Main();
     }
+
+    String command1 = "/cmd1";
+    String command2 = "/cmd2";
+    String command3 = "/cmd3";
+//    String commandx = "cmdx" ;
+
+    String ansver1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    String ansver2 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    String ansver3 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
     public class MyAmazingBot extends TelegramLongPollingBot {
         @Override
         public void onUpdateReceived(Update update) {
 
-//            if (update.hasMessage() && update.getMessage().hasText() && "/status".equals(update.getMessage().getText())) {
-                SendMessage message = new SendMessage()
-                        .setChatId(update.getMessage().getChatId())
-                        .setText("АУЕ!");
-                try {
-                    sendMessage(message);
-                } catch (TelegramApiException e) {
-                    e.printStackTrace();
-                }
-//            }
+            if (containsComand(update, command1)) sendText(update, ansver1);
+            if (containsComand(update, command2)) sendText(update, ansver2);
+            if (containsComand(update, command3)) sendText(update, ansver3);
+        }
+
+        public MyAmazingBot() {
+            super();
+        }
+
+        void sendText(Update update, String text) {
+            SendMessage message = new SendMessage()
+                    .setChatId(update.getMessage().getChatId())
+                    .setText(text);
+
+            try {
+                sendMessage(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+
+        boolean containsComand(Update update, String command) {
+            return (update.hasMessage() && update.getMessage().hasText() && update.getMessage().getText().startsWith(command));
         }
 
         @Override
